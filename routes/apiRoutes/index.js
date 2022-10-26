b1510fa6f74292bc6ce8016ba8d728bc7e830cc0
Emily Hill <emhillmed2021@gmail.com>
2022-10-26 15:03:56 -0500

    created employees js and put it all into a class, unsure about modules.export and if the functions in viewEmployee.js are correct

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,42 +1,8 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const express = require('express');
+const router = express.Router();
 
+router.use(require('./viewEmployees'));
+router.use(require('./viewDepartments'));
+router.use(require('./viewDepartments')); 
 
-const promptQuestions = employeeData => {
-    return inquirer
-    .prompt([
-        {
-            type: 'list',
-            name:'action',
-            message: 'What would you like to do?',
-            choices:['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
-        }
-    ])
-    .then(function(userInput) {
-        switch(userInput.action){
-            case 'View All Employees':
-                viewEmployees();
-                break; 
-            case 'Add Employee':
-                addEmployee();
-                break; 
-            case 'Update Enployee Role':
-                updateEmployeeRole();
-                break; 
-            case 'View All Roles':
-                viewRoles();
-                break; 
-            case 'Add Role':
-                addRole();
-                break; 
-            case 'View All Departments':
-                viewDepartments();
-                break; 
-            case 'Add Department':
-                addDepartment();
-                break; 
-        }
-    })
-} 
-
-promptQuestions(); 
+module.export = router; 
