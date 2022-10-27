b09f39f4c6d2d2e6416b43f33bb821436e530ae1
Emily Hill <emhillmed2021@gmail.com>
2022-10-27 13:31:32 -0500

    added logic to api files for get/post/put for department and roles, tried my hand a inquirer prompts for add employee/department/role and update employee role.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,31 @@
 INSERT INTO employee
 (id, first_name, last_name, role_id, manager_id)
 VALUES 
-('1', 'John', 'Doe', '', ''),
-('2', 'Mike', 'Chan', '', ''),
-('3', 'Ashley', 'Rodriguez', '', ''),
-('4', 'Kevin', 'Tupik', '', ''),
-('5', 'Kunal', 'Singh', '', ''),
-('6', 'Malia', 'Brown', '', ''),
-('7', 'Sarah', 'Lourd', '', ''),
-('8', 'Tom', 'Allen', '', ''); 
+('1', 'John', 'Doe', '1'),
+('2', 'Mike', 'Chan', '2', '1'),
+('3', 'Ashley', 'Rodriguez', '3'),
+('4', 'Kevin', 'Tupik', '4', '3'),
+('5', 'Kunal', 'Singh', '5'),
+('6', 'Malia', 'Brown', '6', '5'),
+('7', 'Sarah', 'Lourd', '7'),
+('8', 'Tom', 'Allen', '8', '7'); 
 
 INSERT INTO role
 (id, title, salary, department_id)
 VALUES
-()
+('1', 'Sales Lead', '100000', '1'),
+('2', 'Sales Associate', '80000', '1'),
+('3', 'Cheif Engineer', '300000', '2'),
+('4', 'Software Engineer', '150000', '2'),
+('5', 'Account Manager', '160000', '3'),
+('6', 'Accountant', '130000', '3'),
+('7', 'Head of Legal', '250000', '4'),
+('8', 'Lawyer', '200000', '4'); 
+
+INSERT INTO department
+(id, name)
+VALUES
+('1', 'Sales'),
+('2', 'Engineering'),
+('3', 'Finance'),
+('4', 'Legal'); 
